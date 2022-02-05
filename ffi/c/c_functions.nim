2646f6519a4672eb99337d65d24781f29ae24539
nziokaJimkelly <nziokajimkelly@outlook.com>
2022-02-05 18:33:15 +0300

    Added a few C functions to use with Nim's FFI

--- ffi/c/c_functions.nim
+++ ffi/c/c_functions.nim
@@ -1,2 +1,5 @@
 proc getchar*(): cint {.importc, header: "stdio.h"}
 proc printf*(format: cstring): cint {.importc, varargs, header: "stdio.h"}
+proc malloc*(size: cint): ptr {.importc, header: "stdlib.h"}
+proc free*(`ptr`: ptr): cint {.importc, header: "stdlib.h"}
+proc static_assert*(test: bool, errMsg: cstring): cint {.importc, header: "assert.h"}
